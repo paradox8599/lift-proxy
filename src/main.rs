bb3b0055dc39d251cb0048b7efde93baabe45197
paradox8599 <paradox8599@outlook.com>
2025-02-20 15:46:02 +1100

    refactor: extracted separate functions from closures; added request logging

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use axum::{
     Router,
 };
 use reqwest::{self as r, Error};
+use tracing::info;
 
 fn get_client(proxy_addr: Option<String>, proxy_auth: Option<String>) -> r::Client {
     let mut client = r::Client::builder();
@@ -42,45 +43,45 @@ async fn get_response_stream(resp: Result<r::Response, Error>) -> Response<Body>
     Response::builder().status(status).body(body).unwrap()
 }
 
+async fn proxied_get(
+    Path((proxy_addr, proxy_auth, addr)): Path<(String, String, String)>,
+) -> Response<Body> {
+    let addr = match addr.rfind("/v1") {
+        Some(index) => addr[..index].to_string() + &addr[index + 3..],
+        None => addr,
+    };
+    info!("[POST] {} -> {}", proxy_addr, addr);
+    let client = get_client(Some(proxy_addr), Some(proxy_auth));
+    let url = r::Url::parse(&format!("https://{}", addr)).unwrap();
+    let res = client.get(url).send().await;
+    get_response_stream(res).await
+}
+
+async fn proxied_post(
+    Path((proxy_addr, proxy_auth, addr)): Path<(String, String, String)>,
+    body: String,
+) -> Response<Body> {
+    let addr = match addr.rfind("/v1") {
+        Some(index) => addr[..index].to_string() + &addr[index + 3..],
+        None => addr,
+    };
+    info!("[GET]  {} -> {}", proxy_addr, addr);
+    let client = get_client(Some(proxy_addr), Some(proxy_auth));
+    let url = r::Url::parse(&format!("https://{}", addr)).unwrap();
+    let res = client
+        .post(url)
+        .body(body)
+        .header("Content-Type", "application/json")
+        .send()
+        .await;
+    get_response_stream(res).await
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
-        .route(
-            "/{proxy_addr}/{proxy_auth}/{*addr}",
-            get(
-                |Path((proxy_addr, proxy_auth, addr)): Path<(String, String, String)>| async move {
-                    let addr = match addr.rfind("/v1") {
-                        Some(index) => addr[..index].to_string() + &addr[index + 3..],
-                        None => addr,
-                    };
-                    let client = get_client(Some(proxy_addr), Some(proxy_auth));
-                    let url = r::Url::parse(&format!("https://{}", addr)).unwrap();
-                    let res = client.get(url).send().await;
-                    get_response_stream(res).await
-                },
-            ),
-        )
-        .route(
-            "/{proxy_addr}/{proxy_auth}/{*addr}",
-            post(
-                |Path((proxy_addr, proxy_auth, addr)): Path<(String, String, String)>,
-                 body: String| async move {
-                    let addr = match addr.rfind("/v1") {
-                        Some(index) => addr[..index].to_string() + &addr[index + 3..],
-                        None => addr,
-                    };
-                    let client = get_client(Some(proxy_addr), Some(proxy_auth));
-                    let url = r::Url::parse(&format!("https://{}", addr)).unwrap();
-                    let res = client
-                        .post(url)
-                        .body(body)
-                        .header("Content-Type", "application/json")
-                        .send()
-                        .await;
-                    get_response_stream(res).await
-                },
-            ),
-        );
+        .route("/{proxy_addr}/{proxy_auth}/{*addr}", get(proxied_get))
+        .route("/{proxy_addr}/{proxy_auth}/{*addr}", post(proxied_post));
 
     Ok(router.into())
 }
