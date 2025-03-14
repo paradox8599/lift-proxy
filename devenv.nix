c06f6b48906dba300201f7671d82142bbda44ed3
paradox8599 <paradox8599@outlook.com>
2025-03-14 23:28:20 +1100

    dev: simplified envrc

--- devenv.nix
+++ devenv.nix
@@ -12,13 +12,15 @@
   packages = [
     pkgs.git
     pkgs.openssl
-
+    pkgs.cargo-shuttle
   ];
 
   languages.rust = {
     enable = true;
     channel = "nightly";
   };
+
+  scripts.shuttle.exec = "cargo shuttle $@";
 
   # See full reference at https://devenv.sh/reference/options/
 }
