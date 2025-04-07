e23ac8d595246368f2ca7ddbbd07022ee8b889ec
paradox8599 <paradox8599@outlook.com>
2025-04-07 22:09:56 +1000

    refactor: simplify rust toolchain configuration in flake.nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    # fenix.url = "github:nix-community/fenix";
   };
 
   outputs =
@@ -20,16 +19,29 @@
       let
         name = "lift-proxy";
         version = "0.1.0";
+
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-        rustToolchain = pkgs.rust-bin.stable."1.85.0".default.override {
-          extensions = [
-            "rust-analyzer"
-            "clippy"
-            "rustfmt"
-          ];
-        };
-        # rustToolchain = fenix.packages.${system}.minimal.toolchain;
+
+        nightly = false;
+        extensions = [
+          "clippy"
+          "rustfmt"
+          "rust-src"
+          "rust-docs"
+          "rust-analyzer"
+        ];
+
+        rustToolchain =
+          if nightly then
+            pkgs.rust-bin.selectLatestNightlyWith (
+              toolchain: toolchain.default.override { inherit extensions; }
+            )
+          else
+            pkgs.rust-bin.stable."1.85.0".default.override {
+              inherit extensions;
+            };
+
         rustPlatform = pkgs.makeRustPlatform {
           cargo = rustToolchain;
           rustc = rustToolchain;
